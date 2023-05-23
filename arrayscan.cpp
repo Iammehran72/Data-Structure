49f1028387311cb4ac70a8f77c0f41485b9f309f
Mehran Parvaiz <106099427+Iammehran72@users.noreply.github.com>
2023-05-23 19:33:42 +0530

    Add files via upload

---
+++ arrayscan.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[100], n, i;
+	cout<<"Enter the number of Elements : ";
+	cin>>n;
+	cout<<"Enter The Elements  : ";
+	for(i=0; i<n; i++){
+		cin>>a[i];
+	}
+	for(i=0; i<n; i++){
+	
+	cout<<a[ i ];
+}
+	return 0;
+}
