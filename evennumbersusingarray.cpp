34839c54858de2e6bd29baf121ea6dd0548345b9
Mehran Parvaiz <106099427+Iammehran72@users.noreply.github.com>
2023-05-23 19:33:57 +0530

    Add files via upload

---
+++ evennumbersusingarray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[100], n, i=0;
+	cout<<"Enter the number of Elements : ";
+	cin>>n;
+	cout<<"Enter The Elements  : ";
+	for(i=0; i<n; i++){
+		cin>>a[i];
+	}
+	for(i=0; i<n; i++)
+	{
+		if(a[i] % 2==0)
+		{
+			cout<<a[i]
+		;}
+	}
+}
